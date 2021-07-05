b301e2461a787fcb7de52590ec6533dc66a7da3f
Shiven <shiven.assetmonk@gmail.com>
2021-07-05 15:38:26 +0530

    change in line 16

--- file1.js
+++ file1.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
+//Changes made here
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     console.log("done");
